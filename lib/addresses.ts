ade01a205fc172aa3e89605fa3188f18aa4a9b79
Matt Weichel <maweiche@gmail.com>
2022-04-15 10:11:31 -0400

    store functional for Sol transactions

---
+++ lib/addresses.ts
@@ -0,0 +1,4 @@
+import { PublicKey } from "@solana/web3.js"
+
+// insert shop wallet address
+export const shopAddress = new PublicKey('8YB9vvdKu1LbZqf9po8MUtUATbUDLAtNEvZviYgZpygv')
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
 
 export default function HomePage() {
+
+  const { publicKey } = useWallet()
+
   return (
     <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
       <SiteHeading>Cookies Inc</SiteHeading>
 
-      <Products submitTarget='/checkout' enabled={true} />
+      {/* solana wallet collect button */}
+      <div className="basis-1/4">
+        <WalletMultiButton className='!bg-gray-900 hover:scale-105' />
+      </div>
+
+      {/* disable checking out without wallet connected */}
+      <Products submitTarget='/checkout' enabled={publicKey !== null} />
     </div>
   )
 }
