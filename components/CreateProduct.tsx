fdbdc450ddaac9e3ca8ce84cd799df2604ed45a4
Matt Weichel <maweiche@gmail.com>
2022-04-25 16:00:27 -0400

    updated console log for object's key value to display url

--- components/CreateProduct.tsx
+++ components/CreateProduct.tsx
@@ -74,7 +74,8 @@ const CreateProduct = (props: any) => {
         <button
             className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={() => {
-                console.log(file.url);
+                //file returns keys, need to access keys, then just target the url in index
+                console.log(Object.values(file)[1]);
                 // uploadLinks.push(file.url);
                 // console.log("these are the upload links", ...uploadLinks);
             }}
